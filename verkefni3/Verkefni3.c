84040d22e48b4532bc4adeae69526153f93e7fe5
bjarkifannar <bjarkifannar@gmail.com>
2018-10-23 15:08:49 +0000

    Verkefni 3

--- verkefni3/Verkefni3.c
+++ verkefni3/Verkefni3.c
@@ -1,23 +1,53 @@
 #pragma config(Motor,  port4, leftMotor,    tmotorNormal, openLoop, reversed)
 #pragma config(Motor,  port6, rightMotor,   tmotorNormal, openLoop)
 #pragma config(Motor,  port5, armMotor, tmotorNormal, openLoop, reversed)
+#pragma config(Motor,  port3, clawMotor, tmotorNormal, openLoop, reversed)
 #pragma config(Sensor, dgtl5,	leftEncoder,  sensorQuadEncoder)
 #pragma config(Sensor, dgtl7,	rightEncoder, sensorQuadEncoder)
 #pragma config(Sensor, dgtl12, touchSensor, sensorTouch)
 #pragma config(Sensor, in1, potentiometer, sensorPotentiometer)
 
 #include "../GlobalFunctions/Functions.c"
+
+const int deadzone = 10;
+
 //ATH!! connection virkadi aldrei thannig thessi kodi gaeti eda gaeti ekki virkad.
 task main()
 {
-	StartTask(stopButton);
-	StartTask(holdArm);
+	//StartTask(stopButton);
+
 	while(1 == 1)
   {
-    //Right side of the robot is controlled by the right joystick, Y-axis
-    motor[rightMotor] = vexRT[Ch2];
-    //Left side of the robot is controlled by the left joystick, Y-axis
+    //Driving controlled by left stick
+    /*motor[rightMotor] = vexRT[Ch3];
     motor[leftMotor] = vexRT[Ch3];
+
+    //Turning controlled by right stick
+    int tmpRightStickX = vexRT[Ch1];
+    writeDebugStreamLine("%d", tmpRightStickX);
+
+    if(tmpRightStickX < -deadzone)
+    {
+    	motor[rightMotor] = tmpRightStickX;
+    	motor[leftMotor] = -tmpRightStickX;
+    }
+    else if(tmpRightStickX > deadzone)
+    {
+    	motor[rightMotor] = -tmpRightStickX;
+    	motor[leftMotor] = tmpRightStickX;
+    }*/
+
+    motor[rightMotor] = vexRT[Ch3] - vexRT[Ch4];
+    motor[leftMotor] = vexRT[Ch3] + vexRT[Ch4];
+
+    motor[armMotor] = vexRT[Ch2];
+
+    if(vexRT[Btn6U])
+    	motor[clawMotor] = 60;
+   	else if(vexRT[Btn5U])
+   		motor[clawMotor] = -40;
+   	else
+   		motor[clawMotor] = 0;
   }
 
 	StopAllTasks();
